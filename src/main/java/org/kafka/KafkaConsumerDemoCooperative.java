b95aa550b08a96c809c882dfc585de3fef040776
anuradha <anuradha.kadurugasyaya@gmail.com>
2024-04-16 07:16:43 +0800

    Created kafka consumers

---
+++ src/main/java/org/kafka/KafkaConsumerDemoCooperative.java
@@ -0,0 +1,83 @@
+package org.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.CooperativeStickyAssignor;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+@Slf4j
+public class KafkaConsumerDemoCooperative {
+
+    public static void main(String[] args) {
+
+        log.info("Kafka consumer");
+
+        String groupId = "kafka-demo";
+
+        Properties properties = new Properties();
+
+        // Connect to localhost
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // Set consumer properties
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+
+        properties.setProperty("group.id", groupId);
+
+        properties.setProperty("auto.offset.reset", "earliest");
+        properties.setProperty("partition.assignment.strategy", CooperativeStickyAssignor.class.getName());
+
+        // Create consumer
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+
+        // Get reference to the main thread
+        final Thread mainThread = Thread.currentThread();
+
+        // Adding the shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("Shutting down the consumer.");
+                kafkaConsumer.wakeup();
+
+                // Join the main thread to allow the execution of the code in the main thread
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    log.error(e.getMessage());
+                }
+            }
+        });
+
+        try {
+            // Subscribe to a topic
+            kafkaConsumer.subscribe(List.of("kafka_topic_1"));
+
+            //Poll for data
+            while (true) {
+                log.info("Polling");
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(1000));
+
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info("Key: {}, Value: {}", record.key(), record.value());
+                    log.info("Partition: {}, Offset: {}", record.partition(), record.offset());
+                }
+            }
+        } catch (WakeupException e) {
+            log.info("Consumer is starting to shut down");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        } finally {
+            log.info("Shutting down the consumer");
+            kafkaConsumer.close();
+        }
+
+    }
+}
